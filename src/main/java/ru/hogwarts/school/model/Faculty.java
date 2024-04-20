805a102057334bec13ae6dcea908e5f9081e6e4c
Aleksey-Gavrin <darkgarvin@gmail.com>
2024-04-20 21:05:50 +0300

    6th app tested with TestRestTemplate

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 public class Faculty {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name, color;
 
